03e7502d2d6161c12d268cadd5b6ce3f78f63e89
mcmaster <devnull@localhost>
2013-04-19 16:29:49 -0400

    Disable refresh following

--- rodham/plugins/htmltitle.py
+++ rodham/plugins/htmltitle.py
@@ -3,17 +3,18 @@ import re
 
 def get_title(url):
     br = mechanize.Browser()
-    br.open(url)
+    br.set_handle_refresh(False)
+    br.open(url, timeout=5)
     return br.title()
 
 class HtmlTitlePlugin(object):
     def proc(self, M):
-        m = re.search(r"(https?://[^\s]+)", M["body"])
+        m = re.search(r"(https?://\S+)", M["body"])
         if not m:
             return
         url = m.groups()[0]
 
-        try:
-            M.reply("%s || %s" % (url,get_title(url))).send()
-        except:
-            pass
+        #try:
+        M.reply("%s || %s" % (url,get_title(url))).send()
+        #except:
+        #    pass
