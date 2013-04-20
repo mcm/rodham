83a0eacb56ec2146c615f0b997e8df9bdeec50f0
mcmaster <devnull@localhost>
2013-04-20 12:50:34 -0400

    Revert sender detection for neat

--- rodham/plugins/neat.py
+++ rodham/plugins/neat.py
@@ -2,7 +2,10 @@ import re
 
 class NeatPlugin(object):
     def proc(self, M):
-        sender = M.sender
+        if M["type"] == "groupchat":
+            sender = M.get_from().resource
+        else:
+            sender = M.get_from().user
         if re.search(r"!neat", M["body"]):
             M.reply("Wow %s that IS neat!" % sender).send()
 
