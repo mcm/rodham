d365ae0fe7bec8e0d5b1cd9cadc5d2ceca522e65
mcmaster <devnull@localhost>
2013-04-20 13:09:59 -0400

    Deeper detection of sender for swearjar

--- rodham/plugins/swearjar.py
+++ rodham/plugins/swearjar.py
@@ -41,12 +41,29 @@ class SwearJarPlugin(object):
             Swear.create_table()
         self.update_words()
         self.admins = conf["admin_users"]
+        self.bot = kwargs["bot"]
 
     def update_words(self):
         self.swear_words = CostSortedDict([(s.word,s.cost) for s in SwearWord.select()])
 
+    def get_nick(self, name, room):
+        if room is None:
+            return name
+        jid = "%s@%s" % (name, self.bot.jid.domain)
+        muc = self.bot.plugin["xep_0045"]
+        for nick in muc.getRoster(room):
+            if muc.rooms[room][nick]["jid"].bare == jid:
+                return nick
+        return name
+
     def proc(self, M):
         sender = M.sender
+        if M["type"] == "groupchat":
+            nick = M["mucnick"]
+            room = M["mucroom"]
+        else:
+            nick = sender
+            room = None
 
         if sender == "":
             return
@@ -107,7 +124,8 @@ class SwearJarPlugin(object):
                     if not leaders.has_key(swear.who):
                         leaders[swear.who] = 0.0
                     leaders[swear.who] += swear.cost
-                M.reply("\n".join([ "{}: ${:,.2f}".format(k,v) for (k,v) in leaders.sorteditems() ])).send()
+                leaders = [ (self.get_nick(k, room),v) for (k,v) in leaders.sorteditems() ]
+                M.reply("\n".join([ "{}: ${:,.2f}".format(k,v) for (k,v) in leaders ])).send()
             else:
                 M.reply("Not yet implemented...").send()
             return
@@ -123,4 +141,4 @@ class SwearJarPlugin(object):
                 for c in range(0, len(words)):
                     Swear(who=sender, word=swear_word, cost=cost).save()
         if owed > 0.0:
-            M.reply("Shame on you {}, ${:,.2f} to the swear jar".format(sender, owed)).send()
+            M.reply("Shame on you {}, ${:,.2f} to the swear jar".format(nick, owed)).send()
