1c9c1b097af9dbff0fe4a1c505695e1e00597c82
mcmaster <devnull@localhost>
2013-04-22 13:39:16 -0400

    Additional rules support

--- rodham/plugins/rules.py
+++ rodham/plugins/rules.py
@@ -1,10 +1,24 @@
+import os
+import re
+
 class RulesPlugin(object):
+    def __init__(self, *args, **kwargs):
+        self.conf = kwargs["conf"]
+
     def proc(self, M):
-        if M["body"] != "!rules":
+        m = re.match("^!rules(?: (\S+))?", M["body"], flags=re.I)
+        if not m:
             return
 
-        #TODO: Fix me
-        with open("/opt/virtual_envs/rodham/conf/roddy_rules.txt", "r") as f:
+        which = m.groups()[0]
+        if which is None:
+            which = self.conf.get("default", "roddy")
+
+        fn = self.conf.get(which, False)
+        if not fn or not os.path.exists(fn):
+            return
+
+        with open(fn, "r") as f:
             rules = f.read()
 
         M.reply(rules.strip()).send()
