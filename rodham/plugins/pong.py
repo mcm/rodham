902970aca37a7287325931ac8709feb979b1305a
Steve McMaster <mcmaster@hurricanelabs.com>
2014-03-11 18:49:13 -0400

    Every few messages, send wang chung video

--- rodham/plugins/pong.py
+++ rodham/plugins/pong.py
@@ -1,6 +1,13 @@
 class PongPlugin(object):
+    wang = 0
+
     def proc(self, M):
         if M["body"] == "ping":
             M.reply("pong").send()
         elif M["body"] == "wang":
-            M.reply("chung").send()
+            self.wang += 1
+            if self.wang > 3:
+                self.wang = 1
+                M.reply("http://goo.gl/MXrQ").send()
+            else:
+                M.reply("chung").send()
