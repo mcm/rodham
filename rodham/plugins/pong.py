3b950a2b3964bf563c86b1057b391223f5fc4147
Steve McMaster <mcmaster@hurricanelabs.com>
2014-03-11 18:43:00 -0400

    Wang Chung

--- rodham/plugins/pong.py
+++ rodham/plugins/pong.py
@@ -2,3 +2,5 @@ class PongPlugin(object):
     def proc(self, M):
         if M["body"] == "ping":
             M.reply("pong").send()
+        elif M["body"] == "wang":
+            M.reply("chung").send()
